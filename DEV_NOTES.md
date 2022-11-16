6f75b0b307383f9bd3c4363fda39f0e293b043c2
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2022-11-16 16:07:03 +0100

    Add cross-file navigation

---
+++ DEV_NOTES.md
@@ -0,0 +1,3 @@
+1. Basic debug URL: https://blocks.githubnext.com/Krzysztof-Cieslak/RustSample?path=src%2Fmain.rs&devServer=http%3A%2F%2Flocalhost%3A4000%2F&branch=main&blockKey=Krzysztof-Cieslak__IDE-Block__ide-block&fileRef=
+
+2. Cross-file nav URL: https://blocks.githubnext.com/Krzysztof-Cieslak/CSharpSample?path=Program.cs&devServer=http%3A%2F%2Flocalhost%3A4000%2F&branch=main&blockKey=Krzysztof-Cieslak__IDE-Block__ide-block&fileRef=
--- blocks/ide-block/index.tsx
+++ blocks/ide-block/index.tsx
@@ -6,10 +6,7 @@ import {
   getLanguageFromFilename,
 } from "@githubnext/blocks";
 import "./index.css";
-import Editor, {
-  useMonaco,
-  Monaco,
-} from "@monaco-editor/react";
+import Editor, { useMonaco, Monaco } from "@monaco-editor/react";
 import {
   CancellationToken,
   editor,
@@ -21,11 +18,35 @@ import {
 } from "monaco-editor";
 import { useEffect } from "react";
 import { LsifReader, UriTransformer } from "@ionide/lsif-reader";
+import * as lsp from "vscode-languageserver-types";
+
+let getDirName = (file: string) => {
+  let parts = file.split("/");
+  parts.pop();
+  return parts.join("/");
+};
+
+let pathJoin = (...parts: string[]) => {
+  let separator = "/";
+  let np = parts.map((p) => (p.startsWith(separator) ? p.substring(1) : p));
+  return np.join(separator);
+};
+
+let DEBUG = true;
+let logger = DEBUG ? console.log : () => {};
 
 const rawUrl = "https://raw.githubusercontent.com";
 const getUrl = (repo: string, file: string, branch: string) =>
   `${rawUrl}${repo}/${branch}/${file}`;
 let lsifReader = new LsifReader();
+
+let workspaceRoot = () => {
+  let wr = lsifReader.getWorkspaceRoot();
+  if (wr) {
+    return wr.endsWith(".toml") ? getDirName(wr) : wr;
+  }
+  return undefined;
+};
 
 const hoverProvider: languages.HoverProvider = {
   provideHover: (
@@ -33,6 +54,7 @@ const hoverProvider: languages.HoverProvider = {
     position: Position,
     token: any
   ): languages.ProviderResult<languages.Hover> => {
+    logger("hover", model.uri, position);
     let result = lsifReader.hover(model.uri.toString(true), {
       line: position.lineNumber - 1,
       character: position.column - 1,
@@ -40,6 +62,8 @@ const hoverProvider: languages.HoverProvider = {
     if (!result || !result.contents || !result.range) {
       return;
     }
+    logger("hover result", result);
+
     let contents: IMarkdownString[];
     if (typeof result.contents === "string") {
       contents = [{ value: result.contents }];
@@ -64,13 +88,24 @@ const hoverProvider: languages.HoverProvider = {
   },
 };
 
-let mapLocation = (uri: Uri, l: any): languages.Location => {
+let mapLocation = (
+  modelUri: Uri,
+  workspaceRoot: string | undefined,
+  l: lsp.Location
+): languages.Location => {
   let range = {
     startLineNumber: l.range.start.line + 1,
     startColumn: l.range.start.character + 1,
     endLineNumber: l.range.end.line + 1,
     endColumn: l.range.end.character + 1,
   };
+  let uri =
+    workspaceRoot === undefined
+      ? modelUri
+      : l.uri.startsWith(workspaceRoot)
+      ? Uri.file(l.uri.substring(workspaceRoot.length))
+      : modelUri;
+
   return { uri: uri, range: range };
 };
 
@@ -84,7 +119,7 @@ let mapRange = (l: any): IRange => {
 };
 
 let mapDocumentSymbol = (s: any): languages.DocumentSymbol => {
-  let ds : languages.DocumentSymbol = {
+  let ds: languages.DocumentSymbol = {
     name: s.name,
     detail: s.detail,
     kind: s.kind,
@@ -105,23 +140,31 @@ let definitionProvider: languages.DefinitionProvider = {
       line: position.lineNumber - 1,
       character: position.column - 1,
     });
+    logger("Hover res", result);
     if (!result) {
       return;
     }
 
+    let wr = workspaceRoot();
+
     let locations =
       result instanceof Array
-        ? result.map((l) => mapLocation(model.uri, l))
-        : [mapLocation(model.uri, result)];
+        ? result.map((l) => mapLocation(model.uri, wr, l))
+        : [mapLocation(model.uri, wr, result)];
+
+    logger("definitionProvider", locations);
     return locations;
   },
 };
 
 let documentSymbolProvider: languages.DocumentSymbolProvider = {
-  provideDocumentSymbols: (model: editor.ITextModel, token: CancellationToken): languages.ProviderResult<languages.DocumentSymbol[]> => {
-    console.log("provideDocumentSymbols");
+  provideDocumentSymbols: (
+    model: editor.ITextModel,
+    token: CancellationToken
+  ): languages.ProviderResult<languages.DocumentSymbol[]> => {
+    logger("provideDocumentSymbols");
     let symbols = lsifReader.documentSymbols(model.uri.toString(true));
-    console.log(symbols);
+    logger(symbols);
     if (!symbols) {
       return;
     }
@@ -130,10 +173,9 @@ let documentSymbolProvider: languages.DocumentSymbolProvider = {
       return mapDocumentSymbol(s);
     });
 
-
     return result;
-  }
-}
+  },
+};
 
 let referencesProvider: languages.ReferenceProvider = {
   provideReferences: (
@@ -142,6 +184,7 @@ let referencesProvider: languages.ReferenceProvider = {
     context: languages.ReferenceContext,
     token: any
   ): languages.ProviderResult<languages.Location[]> => {
+    logger("provideReferences", context);
     let result = lsifReader.references(
       model.uri.toString(true),
       {
@@ -154,10 +197,14 @@ let referencesProvider: languages.ReferenceProvider = {
       return;
     }
 
+    let wr = workspaceRoot();
+
     let locations =
       result instanceof Array
-        ? result.map((l) => mapLocation(model.uri, l))
-        : [mapLocation(model.uri, result)];
+        ? result.map((l) => mapLocation(model.uri, wr, l))
+        : [mapLocation(model.uri, wr, result)];
+
+    logger("REF locations", locations);
     return locations;
   },
 };
@@ -180,10 +227,11 @@ let highlightProvider: languages.DocumentHighlightProvider = {
       return;
     }
 
+    let wr = workspaceRoot();
     let locations =
       result instanceof Array
-        ? result.map((l) => mapLocation(model.uri, l))
-        : [mapLocation(model.uri, result)];
+        ? result.map((l) => mapLocation(model.uri, wr, l))
+        : [mapLocation(model.uri, wr, result)];
     return locations.map((l) => {
       return {
         range: l.range,
@@ -193,29 +241,17 @@ let highlightProvider: languages.DocumentHighlightProvider = {
   },
 };
 
-let getDirName = (file: string) => {
-  let parts = file.split("/");
-  parts.pop();
-  return parts.join("/");
-};
-
-let pathJoin = (...parts: string[]) => {
-  return parts.join("/");
-};
-
-let transformerFactory = (
-  workspaceRoot: string | undefined,
-  context: FileContext
-): UriTransformer => {
+let transformerFactory = (context: FileContext): UriTransformer => {
   return {
     toDatabase: (_uri) => {
-      workspaceRoot = workspaceRoot?.endsWith(".toml")
-        ? getDirName(workspaceRoot)
-        : workspaceRoot;
-      if (!workspaceRoot) {
+      logger("toDatabase", _uri);
+      let wr = workspaceRoot();
+      if (!wr) {
         return _uri;
       }
-      let result = pathJoin(workspaceRoot, context.path);
+      let uri = Uri.parse(_uri);
+      let result = pathJoin(wr, uri.path);
+      logger("toDatabase", result);
       return result;
     },
     fromDatabase: (uri) => {
@@ -232,7 +268,7 @@ let beforeMountHandler = async (monaco: Monaco, context: FileContext) => {
   );
   let res = await fetch(indexUrl);
   let text = await res.text();
-  lsifReader.load(text, (wr) => transformerFactory(wr, context));
+  lsifReader.load(text, (wr) => transformerFactory(context));
 };
 
 let inited = false;
@@ -254,8 +290,14 @@ export default function (props: FileBlockProps) {
       monaco.languages.registerHoverProvider("*", hoverProvider);
       monaco.languages.registerDefinitionProvider("*", definitionProvider);
       monaco.languages.registerReferenceProvider("*", referencesProvider);
-      monaco.languages.registerDocumentSymbolProvider("*", documentSymbolProvider);
-      monaco.languages.registerDocumentHighlightProvider("*", highlightProvider);
+      monaco.languages.registerDocumentSymbolProvider(
+        "*",
+        documentSymbolProvider
+      );
+      monaco.languages.registerDocumentHighlightProvider(
+        "*",
+        highlightProvider
+      );
       inited = true;
     }
   }, [monaco]);
@@ -268,8 +310,82 @@ export default function (props: FileBlockProps) {
       options={{
         readOnly: true,
       }}
+      path={context.path}
       beforeMount={async (monaco: Monaco) => {
         return await beforeMountHandler(monaco, context);
+      }}
+      onMount={(e, monaco) => {
+        logger("onMount", e);
+
+        const editorService = (e as any)._codeEditorService;
+        const openEditorBase = editorService.openCodeEditor.bind(editorService);
+
+        editorService.openCodeEditor = async (input: any, source: any) => {
+          const result = await openEditorBase(input, source);
+          if (result === null) {
+            logger("Open definition for:", input);
+            let uri = input.resource as Uri;
+
+            //navigate to input selection on given model
+            let navigate = (model: editor.ITextModel) => {
+              e.revealRangeInCenterIfOutsideViewport(
+                {
+                  startLineNumber: input.options.selection.startLineNumber,
+                  endLineNumber: input.options.selection.endLineNumber,
+                  startColumn: input.options.selection.startColumn,
+                  endColumn: input.options.selection.endColumn,
+                },
+                editor.ScrollType.Smooth
+              );
+              let word = model.getWordAtPosition({
+                lineNumber: input.options.selection.startLineNumber,
+                column: input.options.selection.startColumn,
+              });
+              if (word) {
+                e.setSelection(
+                  {
+                    startLineNumber: input.options.selection.startLineNumber,
+                    endLineNumber: input.options.selection.endLineNumber,
+                    startColumn: word.startColumn,
+                    endColumn: word.endColumn,
+                  },
+                  source
+                );
+              } else {
+                e.setSelection({
+                  startLineNumber: input.options.selection.startLineNumber,
+                  endLineNumber: input.options.selection.endLineNumber,
+                  startColumn: input.options.selection.startColumn,
+                  endColumn: input.options.selection.endColumn,
+                }),
+                  source;
+              }
+            };
+
+            //Blocks navigation forces reload, I don't think we want that
+            // let path = uri.path.startsWith("/")
+            //   ? uri.path.substring(1)
+            //   : uri.path;
+            // props.onNavigateToPath(path);
+            let model = monaco.editor.getModel(uri);
+            if (model) {
+              e.setModel(model);
+              navigate(model);
+            } else {
+              let url = getUrl(
+                "/" + context.owner + "/" + context.repo,
+                uri.path,
+                "main" //TODO: Context doesn't have branch
+              );
+              let res = await fetch(url);
+              let text = await res.text();
+              let model = monaco.editor.createModel(text, language, uri);
+              e.setModel(model);
+              navigate(model);
+            }
+          }
+          return result; // always return the base result
+        };
       }}
     />
   );
